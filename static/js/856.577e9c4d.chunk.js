23f06046f4ee5b934ebb234d0b8a66e09b402492
imprex <imprez370@gmail.com>
2023-11-29 16:22:36 -0500

    Updates

---
+++ static/js/856.577e9c4d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkheyyy_genpg=self.webpackChunkheyyy_genpg||[]).push([[856],{856:(t,c,e)=>{e.d(c,{Tmq:()=>h});var a=e(983);function h(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"}}]})(t)}}}]);
+//# sourceMappingURL=856.577e9c4d.chunk.js.map
